2334fdd51a7ff1bac3d844b80d657339c8b52081
Tyler Smith <tds170000@utdallas.edu>
2022-04-25 02:42:51 -0500

    Initial Functioning Build

--- Assets/Scripts/TerminalC.cs
+++ Assets/Scripts/TerminalC.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TerminalC : MonoBehaviour
 {
+    [SerializeField] [TextArea] private string[] itemInfo = null;
+    [SerializeField] private float textSpeed = 0.01f;
+
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI itemInfoText = null;
+    private int currentDisplayingText = 0;
+
     public void ScanTerminalC()
     {
+        StartCoroutine(AnimateText());
         Debug.Log("Among Us");
+    }
+
+    IEnumerator AnimateText()
+    {
+        for (int i = 0; i < itemInfo[currentDisplayingText].Length + 1; i++)
+        {
+            itemInfoText.text = itemInfo[currentDisplayingText].Substring(0, i);
+            yield return new WaitForSecondsRealtime(textSpeed);
+        }
     }
 }
